4992a29815c12f5eb430796b8bd74ddddbc5c558
nnecec <nnecec@outlook.com>
2022-05-26 14:09:34 +0800

    add prettier types

--- packages/prettier/index.js
+++ packages/prettier/index.js
@@ -1,8 +1,11 @@
+/**
+ * @type {import('prettier').Config}
+ */
 module.exports = {
   semi: false,
   tabWidth: 2,
   singleQuote: true,
-  arrowParens: 'avoid',
-  trailingComma: 'none',
-  plugins: [require('prettier-plugin-tailwindcss')]
-}
+  arrowParens: "avoid",
+  trailingComma: "none",
+  plugins: [require("prettier-plugin-tailwindcss")],
+};
